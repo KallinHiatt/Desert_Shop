7cc7119df07d2c55427513cd6f20b4164631beb6
KallinHiatt <kallin.hiatt@ucas-edu.net>
2024-11-23 22:38:39 +0000

    information

--- dessert.py
+++ dessert.py
@@ -1,106 +1,70 @@
-class DesertItem:
-    def __init__(self, name):
+from abc import ABC, abstractmethod
+
+class DessertItem(ABC):
+    def __init__(self, name, tax_percent = 7.25):
         self.name = name
+        self.tax_percent = 7.25
     def __str__(self):
-        return self.name
+        return f"You ordered a {self.name}"
     def get_name(self):
         return self.name
-class Candy(DesertItem):
-    def __init__(self, name, candy_weight, price_per_pound):
-        super().__init__(name)
+    def calculate_tax(self):
+        return self.calculate_cost() * (self.tax_percent/100)
+
+    @abstractmethod
+    def calculate_cost(self):
+        pass
+    
+class Candy(DessertItem):
+    def __init__(self, name, candy_weight, price_per_pound, tax_percent):
+        super().__init__(name, tax_percent)
         self.candy_weight = candy_weight
         self.price_per_pound = price_per_pound
-    def get_weight(self):
+    def get_candy_weight(self):
         return self.candy_weight
     def get_price_per_pound(self):
         return self.price_per_pound
-#------------------------------------------------
-class Cookie(DesertItem):
-    def __init__(self, name, quantity, price_per_dozen):
-        super().__init__(name)
-        self.quantity = quantity
+    def calculate_cost(self):
+        return self.candy_weight * self.price_per_pound
+    
+class Cookie(DessertItem):
+    def __init__(self, name, cookie_quantity, price_per_dozen, tax_percent):
+        super().__init__(name, tax_percent)
+        self.cookie_quantity = cookie_quantity
         self.price_per_dozen = price_per_dozen
-    def get_quantity(self):
-        return self.quantity
-    def get_price(self):
+    def get_cookie_quantity(self):
+        return self.cookie_quantity
+    def get_price_per_dozen(self):
         return self.price_per_dozen
-#-------------------------------------------------
-class IceCream(DesertItem):
-    def __init__(self, name, scoop_counts, price_per_scoop):
-        super().__init__(name)
-        self.scoop_counts = scoop_counts
+    def calculate_cost(self):
+        return self.cookie_quantity * (self.price_per_dozen/12)
+    
+    
+class IceCream(DessertItem):
+    def __init__(self, name, scoop_count, price_per_scoop, tax_percent):
+        super().__init__(name, tax_percent)
+        self.scoop_count = scoop_count
         self.price_per_scoop = price_per_scoop
-    def get_scoop_counts(self):
-        return self.scoop_counts
-    def get_price(self):
-        return self.price_per_scoop
-#-------------------------------------------------
+    def get_scoop_count(self):
+        return self.scoop_count
+    def get_price_per_scoop(self):
+        return self. price_per_scoop
+    def calculate_cost(self):
+        return self.scoop_count * self.price_per_scoop
+    
+
 class Sundae(IceCream):
-    def __init__(self, name, scoop_counts, price_per_scoop, topping_name, topping_price):
-        super().__init__(name, scoop_counts, price_per_scoop)
+    def __init__(self, name, scoop_count, price_per_scoop, topping_name, topping_price, tax_percent):
+        super().__init__(name, scoop_count, price_per_scoop, tax_percent)
         self.topping_name = topping_name
         self.topping_price = topping_price
-
     def get_topping_name(self):
         return self.topping_name
     def get_topping_price(self):
         return self.topping_price
-        
-def main():
-    stuff = []
-    candy_corn = Candy("Candy Corn", 1.5, .25)
-    gummy_bear = Candy("Gummy Bears", .25, .35)
-    chocolate_chip = Cookie("Chocolate Chip", 6, 3.99)
-    icecream = IceCream("Pistachio", 2, .79)
-    vanilla = Sundae("Vanilla", 3, .69, "Hot Fudge", 1.29)
-    oatmeal_rasin = Cookie("Oatmeal Raisin", 2, 3.45)
+    def calculate_cost(self):
+        return self.scoop_count * self.price_per_scoop + self.topping_price
     
-    can_order = [
-    candy_corn,
-    gummy_bear,
-    chocolate_chip,
-    icecream,
-    vanilla,
-    oatmeal_rasin
-    ]
-    stuff.append(candy_corn.name)
-    stuff.append(gummy_bear.name)
-    stuff.append(chocolate_chip.name) 
-    stuff.append(icecream.name)
-    stuff.append(vanilla.name)
-    stuff.append(oatmeal_rasin.name)
-    
-    print("\n".join(stuff))
-    return f"Total number of items in order: {len(stuff)}"
 
-print(main())
-print()
 
-"""while True:
-    order = input("What would you like to order? (Type: 'cookie', 'candy', 'icecream', or 'sundae')\n")
-    if order == "cookie":
-        print(f"We have {chocolate_chip.name} and {oatmeal_rasin.name} cookies.\n")
-        cookie = input("What cookie would you like? (Type: 'oatmeal' or 'chocolate' or 'nevermind')\n")
-        if cookie == 'oatmeal':
-            print("Here's your oatmeal cookie.")
-            stuff.append(oatmeal_rasin.name)
-        if cookie == 'chocolate':
-            print("Here's your chocolate chip cookie.")
-            stuff.append(chocolate_chip.name)
-        if cookie == "nevermind":
-            continue
-    if order == "candy":
-        print(f"We have {gummy_bear.name} and {candy_corn.name} candy.\n")
-        cookie = input("What cookie would you like? (Type: 'oatmeal' or 'chocolate' or 'nevermind')\n")
-        if cookie == 'oatmeal':
-            print("Here's your oatmeal cookie.")
-            stuff.append(oatmeal_rasin.name)
-        if cookie == 'chocolate':
-            print("Here's your chocolate chip cookie.")
-            stuff.append(chocolate_chip.name)
-        if cookie == "nevermind":
-    if order == "icecream":
-    if order == "sundae":
-        
-    """
 
--- testing/test_dessert.py
+++ testing/test_dessert.py
@@ -1,49 +1,62 @@
-from dessert import DesertItem, Candy, Cookie, IceCream, Sundae
-def test_candy():
-    candy = Candy("Tootsie Rolls", 15.3, 15.00)
+from dessert import DessertItem, Candy, Cookie, IceCream, Sundae
+
+def test_candy_init():
+    candy = Candy("Chocolate", 3.5, 1.5)
     name = candy.get_name()
-    assert name == "Tootsie Rolls"
-    weight = candy.get_weight()
-    assert weight == 15.3
+    assert name == "Chocolate"
+    weight = candy.get_candy_weight()
+    assert weight == 3.5
     price = candy.get_price_per_pound()
-    assert price == 15.00
-    
-#-------------------------------------------------    
-def test_cookie():
-    cookie = Cookie("Chocolate Chip", 2, 2.00 )
+    assert price == 1.5
+    cost = candy.candy_weight * candy.price_per_pound
+    assert candy.calculate_cost() == cost
+    tax = candy.calculate_cost() * (7.25/100)
+    assert candy.calculate_tax == tax
+
+def test_tax_percent():
+    candy1 = Candy("Chocolate", 3.5, 1.5)
+    assert candy1.tax_percent == 7.25
+
+def test_cookie_init():
+    cookie = Cookie("Chocolate chip", 12, 15.60)
     name = cookie.get_name()
-    assert name == "Chocolate Chip"
-    quantity = cookie.get_quantity()
-    assert quantity == 2
-    price = cookie.get_price()
-    assert price == 2.00
-    
-#-------------------------------------------------    
-def test_icecream():
-    icecream = IceCream("Vanilla", 3, 2.00)
+    assert name == "Chocolate chip"
+    quant = cookie.get_cookie_quantity()
+    assert quant == 12
+    price = cookie.get_price_per_dozen()
+    assert price == 15.60
+    cost = cookie.cookie_quantity * (cookie.price_per_dozen/12)
+    assert cookie.calculate_cost() == cost
+    tax = cookie.calculate_cost() * (7.25/100)
+    assert cookie.calculate_tax == tax   
+
+
+def test_iceCream_init():
+    icecream = IceCream("Vanilla", 2, 2.99)
     name = icecream.get_name()
     assert name == "Vanilla"
-    counts = icecream.get_scoop_counts()
-    assert counts == 3
-    price = icecream.get_price()
-    assert price == 2.00
-    
-#-------------------------------------------------  
-def test_sundae():
-    sundae = Sundae("Chocolate Sundae", 3, 2.00, "Chocolate Syrup", 0.50)
+    count = icecream.get_scoop_count()
+    assert count == 2
+    price = icecream.get_price_per_scoop()
+    assert price == 2.99
+    cost = icecream.scoop_count * icecream.price_per_scoop
+    assert icecream.calculate_cost() == cost
+    tax = icecream.calculate_cost() * (7.25/100)
+    assert icecream.calculate_tax == tax
+
+def test_sundae_init():
+    sundae = Sundae("Rocky Road", 2, 3.11, "Cherry", 1.00)
     name = sundae.get_name()
-    assert name == "Chocolate Sundae"
-    counts = sundae.get_scoop_counts()
-    assert counts == 3
-    price = sundae.get_price()
-    assert price == 2.00
-    flavor = sundae.get_topping_name()
-    assert flavor == "Chocolate Syrup"
+    assert name == "Rocky Road"
+    count = sundae.get_scoop_count()
+    assert count == 2
+    price = sundae.get_price_per_scoop()
+    assert price == 3.11
+    topping = sundae.get_topping_name()
+    assert topping == "Cherry"
     topping_price = sundae.get_topping_price()
-    assert topping_price == 0.50
-    
-    
-print(test_candy())
-print(test_cookie())
-print(test_icecream())
-print(test_sundae())
+    assert topping_price == 1.00
+    cost = sundae.scoop_count * sundae.price_per_scoop + sundae.topping_price
+    assert sundae.calculate_cost() == cost
+    tax = sundae.calculate_cost() * (7.25/100)
+    assert sundae.calculate_tax == tax
