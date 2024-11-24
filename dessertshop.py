0b55ef496cb60ee98950d06c3f24c7a0514c10f5
KallinHiatt <kallin.hiatt@ucas-edu.net>
2024-11-24 00:11:56 +0000

    scooby

--- dessertshop.py
+++ dessertshop.py
@@ -19,7 +19,127 @@ class Order():
         for item in self.order:
             tax += item.calculate_cost() * (item.tax_percent/100)
         return tax
-    
+
+class DesertShop():
+    def user_prompt_candy(self):
+        print("Type in the name of the candy (str), the weight in pounds (flt), and the price per pound (flt)")
+        while True:
+            candy_name = input("Candy name: ")
+            if isinstance(candy_name, str):
+                break
+            else:
+                print("That's not a string...")
+                continue
+        while True:
+            weight = float(input("Candy weight: "))
+            if isinstance(weight, float):
+                break
+            else:
+                print("That's not a float...")
+                continue
+        while True:
+            price = float(input("Price per pound: "))
+            if isinstance(price, float):
+                break
+            else:
+                print("That's not a float...")
+                continue
+        candy1 = Candy(candy_name, weight, price)
+        return candy1
+
+    def user_prompt_cookie(self):
+        print("Type in the name of the cookie (str), the amount of cookies (int), and the price per dozen (flt)")
+        while True:
+            cookie_name = input("Cookie name: ")
+            if isinstance(cookie_name, str):
+                break
+            else:
+                print("That's not a string...")
+                continue
+        while True:
+            amount = int(input("Cookie amount: "))
+            if isinstance(amount, int):
+                break
+            else:
+                print("That's not a integer...")
+                continue
+        while True:
+            price12 = float(input("Price per dozen: "))
+            if isinstance(price12, float):
+                break
+            else:
+                print("That's not a float...")
+                continue
+        cookie1 = Cookie(cookie_name, amount, price12)
+        return cookie1
+
+    def user_prompt_icecream(self):
+        print("Type in the name of the icecream (str), the amount of scoops (int), and the price per scoop (flt)")
+        while True:
+            icecream_name = input("Icecream name: ")
+            if isinstance(icecream_name, str):
+                break
+            else:
+                print("That's not a string...")
+                continue
+        while True:
+            scooby_number = int(input("How many scoops: "))
+            if isinstance(scooby_number, int):
+                break
+            else:
+                print("That's not a integer...")
+                continue
+        while True:
+            scooby_price = float(input("Price per scoop: "))
+            if isinstance(scooby_price, float):
+                break
+            else:
+                print("That's not a float...")
+                continue
+        icecream1 = IceCream(icecream_name, scooby_number, scooby_price)
+        return icecream1
+
+    def user_prompt_sundae(self):
+        print("Type in the name of the ice cream(str), the scoop count(int), the price per scoop(float), topping name(str), and topping price(float).")
+        while True:
+            sundae_name = input("Ice cream name: ")
+            if isinstance(sundae_name, str):
+                break
+            else:
+                print("That's not a string. Type in a string.")
+                continue
+        while True:        
+            scoop_num = int(input("Amount of scoops: "))
+            if isinstance(scoop_num, int):
+                break
+            else:
+                print("That's not an integer. Type in an integer.")
+                continue
+        while True:
+            scoop_price = float(input("Price per scoop: "))
+            if isinstance(scoop_price, float):
+                break
+            else:
+                print("That's not a float. Type in a float.")
+                continue
+        while True:
+            topping_name = input("Price per scoop: ")
+            if isinstance(topping_name, str):
+                break
+            else:
+                print("That's not a string. Type in a string.")
+                continue
+        while True:
+            topping_price = float(input("Price per scoop: "))
+            if isinstance(topping_price, float):
+                break
+            else:
+                print("That's not a float. Type in a float.")
+                continue
+        sundae1 = Sundae(sundae_name, scoop_num, scoop_price, topping_name, topping_price)
+        return sundae1
+
+
 def main(Order):
     order1 = Order()
     order1.add(Candy("Candy Corn", 1.5, .25))
